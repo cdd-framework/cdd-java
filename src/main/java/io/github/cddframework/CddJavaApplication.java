b400b17065a62d09eae37f3a9398cebf9e2e61c4
Fabio Meyer <fabio.meyer@gmail.com>
2025-12-27 15:35:39 +0100

    first commit

---
+++ src/main/java/io/github/cddframework/CddJavaApplication.java
@@ -0,0 +1,13 @@
+package io.github.cddframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CddJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CddJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/cddframework/CddJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.cddframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CddJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
